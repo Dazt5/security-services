6bb4f6b27c0696e8dc65c317804bb65c94dca616
Dazt5 <alvarezdavidt15@gmail.com>
2024-10-19 22:06:12 -0500

    feat: add connection between modules, add Spring JPA and database properties

--- app/src/main/kotlin/com/alejandra/security/services/SecurityServicesApplication.kt
+++ app/src/main/kotlin/com/alejandra/security/services/SecurityServicesApplication.kt
@@ -1,12 +1,13 @@
 package com.alejandra.security.services
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @ComponentScan(basePackages = ["com.alejandra"])
+@EnableJpaRepositories(basePackages = ["com.alejandra"])
 class SecurityServicesApplication
 
 fun main(args: Array<String>) {
